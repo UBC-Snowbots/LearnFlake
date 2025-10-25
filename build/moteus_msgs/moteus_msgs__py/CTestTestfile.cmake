acb357705384381f44be3b7fe2f20bd40bb1b73b
aaronrhim <rhimaaron@gmail.com>
2025-10-24 23:42:41 -0700

    temporary faive hand

--- build/moteus_msgs/moteus_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /RoverFlake2/build/moteus_msgs/moteus_msgs__py
-# Build directory: /RoverFlake2/build/moteus_msgs/moteus_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
