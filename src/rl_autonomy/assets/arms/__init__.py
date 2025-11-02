3bf3a67d34844de6b8aa5e4aac8471de4b51926c
Pranav <pranavrajeshkrishnan06@gmail.com>
2025-11-02 14:13:33 -0800

    Attempt 1: Deleted quite a bit of things, changed a few things- all in the attempt of actually seeing a visualization of the robot

--- src/rl_autonomy/assets/arms/__init__.py
+++ src/rl_autonomy/assets/arms/__init__.py
@@ -1,5 +1,4 @@
 from .faive_hand_p0 import FaiveHand
-from .really_simple_robot import ReallySimpleArm
 from .simple_arm import SimpleArm
 
-__all__ = ["SimpleArm", "FaiveHand", "ReallySimpleArm"]
+__all__ = ["SimpleArm", "FaiveHand"]
