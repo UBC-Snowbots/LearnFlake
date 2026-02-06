6034716fbc578a94f5b472a0ad9dd67bb8594ba7
Aaron Rhim <rhimaaron@gmail.com>
2026-02-06 13:29:06 -0800

    updated sac to HRL and refactored files

--- src/rl_autonomy/__init__.py
+++ src/rl_autonomy/__init__.py
@@ -1,9 +1,7 @@
-from .assets.arms.really_simple_robot import ReallySimpleArm
-from .assets.arms.simple_arm import SimpleArm
-
-from robosuite.robots import ROBOT_CLASS_MAPPING, FixedBaseRobot
-
-ROBOT_CLASS_MAPPING.setdefault("SimpleArm", FixedBaseRobot)
-ROBOT_CLASS_MAPPING.setdefault("ReallySimpleArm", FixedBaseRobot)
-
-__all__ = ["SimpleArm", "ReallySimpleArm"]
+from .networks import MLP, GaussianActor, DoubleCritic, SkillSelectorV3, SkillConditionedActorV3
+from .memory import GPUReplayBuffer
+from .agent import HierarchicalSACAgentV3
+from .config import Config
+from .env_wrapper import RoboSuiteEnvV3, SubprocVecEnvV3
+from .trainer import train_v3, evaluate_v3
+from .main import main
---
+++ src/rl_autonomy/testing/train_lift_v3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Hierarchical SAC Training for Rover2026 Arm - Version 3
+Shim script pointing to the new refactored package in src/rl_autonomy/
+"""
+import sys
+import os
+
+# Add src to path
+ROOT = os.path.dirname(os.path.abspath(__file__))
+# src/ is at ../.. relative to src/rl_autonomy/testing
+SRC_PATH = os.path.abspath(os.path.join(ROOT, "..", ".."))
+
+if SRC_PATH not in sys.path:
+    sys.path.insert(0, SRC_PATH)
+
+from rl_autonomy.main import main
+
+if __name__ == "__main__":
+    main()
